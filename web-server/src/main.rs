cc0d9103d0abbfbc4419f1712d282949ac25ebc5
Arvinder <ArvinderDhan@gmail.com>
2023-08-06 02:36:58 -0500

    feat(web-server): add initial CLI
    Adds a CLI with one commit arg, `data_dir`, which is used later on for
    storing the repositories that are stored in the forge.

--- web-server/src/main.rs
+++ web-server/src/main.rs
@@ -1,4 +1,14 @@
+use std::path::PathBuf;
+
 use axum::{routing, Router, Server};
+use clap::Parser;
+
+/// Webserver component for the code forge.
+#[derive(clap::Parser, Debug)]
+#[command(version, about, long_about = None)]
+struct Args {
+    data_dir: PathBuf,
+}
 
 fn main() {
     let rt = tokio::runtime::Builder::new_multi_thread()
@@ -7,8 +17,10 @@ fn main() {
         .unwrap();
     rt.block_on(async_main());
 }
+
 async fn async_main() {
     let app = Router::new().route("/", routing::get(|| async { "Home Page" }));
+    let _args = std::sync::Arc::new(Args::parse());
     Server::bind(&"[::1]:4000".parse().unwrap())
         .serve(app.into_make_service())
         .await
