47c58f64775c9745d3c3b97af9c6cf457cf686bb
Arvinder <ArvinderDhan@gmail.com>
2023-08-27 22:57:34 -0500

    refactor(web-server): `commit_log` -> `CommitLog::commit_log`
    Namespacing is nice :)

--- web-server/src/repositories.rs
+++ web-server/src/repositories.rs
@@ -13,35 +13,37 @@ struct Commit {
     message_body: String,
 }
 
-pub(crate) async fn commit_log(
-    args: &Args,
-    entity: &str,
-    repo_name: &str,
-) -> Result<Json<CommitLog>, StatusCode> {
-    let repo = git2::Repository::open_bare(
-        args.data_dir
-            .join(format!("repositories/{entity}/{repo_name}")),
-    )
-    .map_err(|e| match e.code() {
-        git2::ErrorCode::NotFound => StatusCode::NOT_FOUND,
-        e => {
-            panic!("Couldn't open repo {entity}/{repo_name}, got unexpected error: {e:?}")
-        }
-    })?;
-    let mut walk = repo.revwalk().unwrap();
-    walk.push(repo.head().unwrap().peel_to_commit().unwrap().id())
-        .unwrap();
-    let messages: Vec<Commit> = walk
-        .take(10)
-        .map(|oid| {
-            let commit = repo.find_commit(oid.unwrap()).unwrap();
-            let message = commit.message().unwrap_or("(empty commit message)");
-            let [header, body @ ..]: &[&str] = &message.split('\n').collect::<Vec<_>>()[..] else { unreachable!() }; // body is empty in the case where there's no new line
-            Commit {
-                message_header: header.to_string(),
-                message_body: body.join("\n"),
+impl CommitLog {
+    pub(crate) async fn commit_log(
+        args: &Args,
+        entity: &str,
+        repo_name: &str,
+    ) -> Result<Json<CommitLog>, StatusCode> {
+        let repo = git2::Repository::open_bare(
+            args.data_dir
+                .join(format!("repositories/{entity}/{repo_name}")),
+        )
+        .map_err(|e| match e.code() {
+            git2::ErrorCode::NotFound => StatusCode::NOT_FOUND,
+            e => {
+                panic!("Couldn't open repo {entity}/{repo_name}, got unexpected error: {e:?}")
             }
-        })
-        .collect();
-    Ok(Json(CommitLog { commits: messages }))
+        })?;
+        let mut walk = repo.revwalk().unwrap();
+        walk.push(repo.head().unwrap().peel_to_commit().unwrap().id())
+            .unwrap();
+        let messages: Vec<Commit> = walk
+            .take(10)
+            .map(|oid| {
+                let commit = repo.find_commit(oid.unwrap()).unwrap();
+                let message = commit.message().unwrap_or("(empty commit message)");
+                let [header, body @ ..]: &[&str] = &message.split('\n').collect::<Vec<_>>()[..] else { unreachable!() }; // body is empty in the case where there's no new line
+                Commit {
+                    message_header: header.to_string(),
+                    message_body: body.join("\n"),
+                }
+            })
+            .collect();
+        Ok(Json(CommitLog { commits: messages }))
+    }
 }
