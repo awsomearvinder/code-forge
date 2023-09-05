0210e9b3608d607ea6f67031024ee8e23c3693e8
Arvinder <ArvinderDhan@gmail.com>
2023-09-05 00:40:50 -0500

    feat(web-frontend): make the styling center content on webpage

--- web-frontend/src/routes/+layout.svelte
+++ web-frontend/src/routes/+layout.svelte
@@ -9,11 +9,21 @@
 <Nav {styling}/>
 
 <div class = "content" style = "--bg: {styling.bg}">
-    <slot />
+    <main>
+        <slot />
+    </main>
 </div>
 
 <style>
     .content {
         background-color: var(--bg);
+        width: 100%;
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+    }
+    main {
+        width: 80rem;
+        max-width: 100%;
     }
 </style>
