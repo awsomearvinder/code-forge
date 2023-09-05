5bec42e36f154b8223bbe3f8186aaf7a8a504f81
Arvinder <ArvinderDhan@gmail.com>
2023-09-04 23:33:01 -0500

    feat(nav): init
    We initialize a basic layout, and put the navbar in it.

---
+++ web-frontend/src/routes/+layout.svelte
@@ -0,0 +1,18 @@
+<script lang = "ts">
+    import Nav from "./Nav.svelte";
+    let styling = {
+        bg: "white",
+    };
+</script>
+
+<Nav {styling}/>
+
+<div class = "content" style = "--bg: {styling.bg}">
+    <slot />
+</div>
+
+<style>
+    .content {
+        background-color: var(--bg);
+    }
+</style>
---
+++ web-frontend/src/routes/Nav.svelte
@@ -0,0 +1,12 @@
+<nav style="background-color: {styling.bg}">
+  <a href="/"> Home </a>
+</nav>
+
+<style>
+  nav {
+    width: 100%;
+  }
+</style>
+<script lang="ts">
+  export let styling: {bg: string};
+</script>
