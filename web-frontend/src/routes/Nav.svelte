a0e2a890aa7fce0ab9d8bc8cd3333be0c0b34294
Arvinder <ArvinderDhan@gmail.com>
2023-09-05 00:02:28 -0500

    feat(navbar): styling, add margin and border-bottom
    UI design is my passion, and this is GORGEOUS.

--- web-frontend/src/routes/Nav.svelte
+++ web-frontend/src/routes/Nav.svelte
@@ -5,8 +5,11 @@
 <style>
   nav {
     width: 100%;
+    padding: 1rem 2rem;
+    border-bottom: 0.12rem solid grey;
   }
 </style>
+
 <script lang="ts">
   export let styling: {bg: string};
 </script>
