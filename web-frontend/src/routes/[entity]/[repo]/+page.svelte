d4ec1f1624a4c368943043b2229d286487a2ea2c
Arvinder <ArvinderDhan@gmail.com>
2023-09-05 01:31:56 -0500

    feat(web-frontend): add commit id to commit log

--- web-frontend/src/routes/[entity]/[repo]/+page.svelte
+++ web-frontend/src/routes/[entity]/[repo]/+page.svelte
@@ -1,9 +1,13 @@
-<script>
+<script lang="ts">
     import { page } from "$app/stores";
     import { onMount } from "svelte";
     import { writable } from "svelte/store";
-
-    const commits = writable([]);
+    type Commits = {
+        message_header: string,
+        message_body: string,
+        commit_id: string,
+    };
+    const commits = writable([] as Commits[]);
     onMount(async () =>{
         let response = await fetch(`http://localhost:4000/api/${$page.url.pathname.substring(1)}/commits`);
         commits.set((await response.json()).commits);
@@ -14,10 +18,13 @@
 <ul>
     {#each $commits as commit (commit.message_header)}
       <div class = "commit-log">
-          <h3> {commit.message_header} </h3>
+          <div class = "commit-header">
+              <h3 class="commit-title"> {commit.message_header} </h3>
+              <code class = "commit-id"> {commit.commit_id.slice(0, 7)} </code>
+          </div>
           {#if commit.message_body}
               <hr>
-              <code> {commit.message_body.trim()} </code>
+              <code class = "commit-description"> {commit.message_body.trim()} </code>
           {/if}
       </div>
     {/each}
@@ -29,17 +36,22 @@
         padding: 0;
         max-width: 80rem;
     }
+    .commit-header {
+        display: flex;
+        align-items: baseline;
+        justify-content: space-between;
+        width: 100%;
+    }
     .commit-log {
         margin: 1rem 0;
         padding: 0rem;
         border: 0.2rem solid black;
     }
-    .commit-log code {
-        margin: 0.2rem;
+    .commit-description {
         white-space: pre-line;
     }
-    .commit-log h3 {
-        margin: 0.2rem;
+    .commit-title, .commit-id, .commit-description {
+        margin: 0.3rem 0.7rem;
     }
     hr {
         width: 100%;
