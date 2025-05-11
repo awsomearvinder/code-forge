awsomearvinder/code-forge

commit ea7819b2f55208a59582d60cb4a5f59b613e2c17

feat: replace `web-frontend` with `web-server` and templating.
Going to use htmx and basic js instead. I don't feel like using a
framework.

web-server/src/entities.rs
@@ -4,7 +4,7 @@ use crate::{get_entries, Args};
 
 #[derive(serde::Serialize)]
 pub(crate) struct Entities {
-    entities: Vec<Entity>,
+    pub entities: Vec<Entity>,
 }
 
 #[derive(serde::Serialize)]
@@ -14,15 +14,15 @@ pub(crate) struct Entity {
 
 #[derive(serde::Serialize)]
 pub(crate) struct Repos {
-    repos: Vec<Repo>,
+    pub repos: Vec<Repo>,
 }
 
 #[derive(serde::Serialize)]
-struct Repo {
+pub(crate) struct Repo {
     name: String,
 }
 
-pub(crate) async fn entities(args: &Args) -> Json<Entities> {
+pub(crate) async fn entities(args: &Args) -> Entities {
     let entities: Vec<Entity> = get_entries(&args.data_dir.join("repositories/"))
         .await
         .into_iter()
@@ -30,11 +30,11 @@ pub(crate) async fn entities(args: &Args) -> Json<Entities> {
             name: i.to_str().unwrap().to_owned(),
         })
         .collect();
-    Json(Entities { entities })
+    Entities { entities }
 }
 
 impl Entity {
-    pub(crate) async fn repos(args: &Args, entity_name: &str) -> Json<Repos> {
+    pub(crate) async fn repos(args: &Args, entity_name: &str) -> Repos {
         let repo_entry_links =
             get_entries(&args.data_dir.join(format!("repositories/{entity_name}")))
                 .await
@@ -43,8 +43,8 @@ impl Entity {
                     name: i.to_str().unwrap().to_owned(),
                 })
                 .collect();
-        Json(Repos {
+        Repos {
             repos: repo_entry_links,
-        })
+        }
     }
 }

web-server/src/main.rs
@@ -2,6 +2,7 @@ use std::ffi::OsString;
 use std::path::Path;
 use std::path::PathBuf;
 
+use axum::Json;
 use axum::{routing, Router, Server};
 use clap::Parser;
 
@@ -12,6 +13,7 @@ use tokio_stream::StreamExt;
 use tower_http::cors::{Any, CorsLayer};
 
 mod entities;
+mod frontend;
 mod repositories;
 
 /// Webserver component for the code forge.
@@ -65,13 +67,30 @@ async fn get_entries(path: &Path) -> Vec<OsString> {
 async fn async_main() {
     let args = std::sync::Arc::new(Args::parse());
     datadir_init(&args.data_dir).await;
+    let f = std::sync::Arc::new(frontend::Frontend::new(args.clone()));
     let app =
         Router::new()
+            .route("/", routing::get({
+                let f = f.clone();
+                move || async move { f.index().await }
+            }))
+            .route("/entities", routing::get({
+                let f = f.clone();
+                move || async move { f.entities().await }
+            }))
+            .route("/e/:name", routing::get({
+                let f = f.clone();
+                move |axum::extract::Path(name): axum::extract::Path<String>| async move { f.repositories(&name).await }
+            }))
+            .route("/r/:entity/:repo", routing::get({
+                let f = f.clone();
+                move |axum::extract::Path((entity, repo)): axum::extract::Path<(String, String)>, axum::extract::Query(req): axum::extract::Query<CommitLogReq>| async move { f.repository(&entity, &repo, &req).await }
+            }))
             .route(
                 "/api/entities",
                 routing::get({
                     let args = args.clone();
-                    move || async move { entities::entities(&args).await }
+                    move || async move { Json(entities::entities(&args).await) }
                 }),
             )
             .route(
@@ -79,7 +98,7 @@ async fn async_main() {
                 routing::get({
                     let args = args.clone();
                     move |axum::extract::Path(name): axum::extract::Path<String>| async move {
-                        entities::Entity::repos(&args, &name).await
+                        Json(entities::Entity::repos(&args, &name).await)
                     }
                 }),
             )
@@ -89,7 +108,7 @@ async fn async_main() {
                     let args = args.clone();
                     move |axum::extract::Path((name, repo)): axum::extract::Path<(String, String)>,
                           axum::extract::Query(req): axum::extract::Query<CommitLogReq>| async move {
-                        repositories::CommitLog::commit_log(&args, &name, &repo, &req).await
+                        repositories::CommitLog::commit_log(&args, &name, &repo, &req).await.map(|r| Json(r))
                     }
                 }),
             )

web-server/src/repositories.rs
@@ -5,11 +5,11 @@ use crate::Args;
 
 #[derive(serde::Serialize)]
 pub(crate) struct CommitLog {
-    commits: Vec<Commit>,
+    pub commits: Vec<Commit>,
 }
 
 #[derive(serde::Serialize)]
-struct Commit {
+pub(crate) struct Commit {
     message_header: String,
     message_body: String,
     commit_id: String,
@@ -18,9 +18,9 @@ struct Commit {
 #[derive(Debug, serde::Deserialize)]
 pub(crate) struct CommitLogReq {
     #[serde(default)]
-    rev: Option<String>,
+    pub(crate) rev: Option<String>,
     #[serde(default)]
-    increment: i32,
+    pub(crate) increment: i32,
 }
 
 impl CommitLog {
@@ -29,7 +29,7 @@ impl CommitLog {
         entity: &str,
         repo_name: &str,
         req: &CommitLogReq,
-    ) -> Result<Json<CommitLog>, StatusCode> {
+    ) -> Result<CommitLog, StatusCode> {
         let repo = git2::Repository::open_bare(
             args.data_dir
                 .join(format!("repositories/{entity}/{repo_name}")),
@@ -61,7 +61,10 @@ impl CommitLog {
             .map(|oid| {
                 let commit = repo.find_commit(oid.unwrap()).unwrap();
                 let message = commit.message().unwrap_or("(empty commit message)");
-                let [header, body @ ..]: &[&str] = &message.split('\n').collect::<Vec<_>>()[..] else { unreachable!() }; // body is empty in the case where there's no new line
+                let [header, body @ ..]: &[&str] = &message.split('\n').collect::<Vec<_>>()[..]
+                else {
+                    unreachable!()
+                }; // body is empty in the case where there's no new line
                 Commit {
                     message_header: header.to_string(),
                     message_body: body.join("\n"),
@@ -69,6 +72,6 @@ impl CommitLog {
                 }
             })
             .collect();
-        Ok(Json(CommitLog { commits: messages }))
+        Ok(CommitLog { commits: messages })
     }
 }

flake.nix
@@ -61,6 +61,7 @@
             pkgs.nodePackages_latest.svelte-language-server
             pkgs.nodePackages_latest.svelte-check
             pkgs.just
+            pkgs.watchexec
           ]
           ++ web-serverBuildInputs;
       };

justfile
@@ -1,7 +1,4 @@
 port:= "8000"
 run data_dir:
 	#!/usr/bin/env bash
-	cargo run -- {{ data_dir }}&
-	webserver_pid=$!
-	trap "kill $webserver_pid" EXIT
-	(cd web-frontend; npm ci && npm run dev -- --port {{ port }})
+	cargo run -- {{ data_dir }}

templates/base.html
@@ -1,0 +1,36 @@
+<!DOCTYPE HTML>
+<html>
+  <head>
+    <style>
+      body {
+        width: 100%;
+        margin: 0;
+      }
+      nav {
+        width: 100%;
+        border-bottom: 0.25rem solid black;
+        border-color: black;
+        padding: 1rem 0;
+      }
+      nav a {
+        padding: 0 1rem;
+      }
+      main {
+        margin: auto;
+        max-width: 100rem;
+      }
+    </style>
+    {% block head %}
+    {% endblock head %}
+  </head>
+  <body>
+    <nav>
+      <!-- I can't be bothered to make a stylesheet -->
+      <a href="/"> Home </a>
+    </nav>
+    <main>
+      {% block content %} 
+      {% endblock content %}
+    </main>
+  </body>
+</html>

templates/entities.html
@@ -1,0 +1,9 @@
+{% extends "base.html" %}
+{% block content %}
+<h1> Entities: </h1>
+<ul>
+  {% for entity in entities %}
+    <li> <a href="/e/{{entity.name}}"> {{ entity.name }} </a> </li>
+  {% endfor %}
+</ul>
+{% endblock content %}

templates/index.html
@@ -1,0 +1,7 @@
+{% extends "base.html" %}
+{% block head %}
+{% endblock head %}
+
+{% block content %}
+<h1> Home </h1>
+{% endblock content %}

commit 9da4d7215f7f07d98e59837aa1f7560da2a20d94

fix(web-server): Remove warnings for unused imports & redundant closure
Fixes: ea7819b

web-server/src/entities.rs
@@ -1,5 +1,3 @@
-use axum::Json;
-
 use crate::{get_entries, Args};
 
 #[derive(serde::Serialize)]

web-server/src/main.rs
@@ -108,7 +108,7 @@ async fn async_main() {
                     let args = args.clone();
                     move |axum::extract::Path((name, repo)): axum::extract::Path<(String, String)>,
                           axum::extract::Query(req): axum::extract::Query<CommitLogReq>| async move {
-                        repositories::CommitLog::commit_log(&args, &name, &repo, &req).await.map(|r| Json(r))
+                        repositories::CommitLog::commit_log(&args, &name, &repo, &req).await.map(Json)
                     }
                 }),
             )

web-server/src/repositories.rs
@@ -1,4 +1,4 @@
-use axum::{http::StatusCode, Json};
+use axum::http::StatusCode;
 use git2::Oid;
 
 use crate::Args;