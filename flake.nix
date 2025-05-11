awsomearvinder/code-forge

commit ea7819b2f55208a59582d60cb4a5f59b613e2c17

feat: replace `web-frontend` with `web-server` and templating.
Going to use htmx and basic js instead. I don't feel like using a
framework.

flake.nix
@@ -61,6 +61,7 @@
             pkgs.nodePackages_latest.svelte-language-server
             pkgs.nodePackages_latest.svelte-check
             pkgs.just
+            pkgs.watchexec
           ]
           ++ web-serverBuildInputs;
       };

justfile
@@ -1,7 +1,4 @@
 port:= "8000"
 run data_dir:
 	#!/usr/bin/env bash
-	cargo run -- {{ data_dir }}&
-	webserver_pid=$!
-	trap "kill $webserver_pid" EXIT
-	(cd web-frontend; npm ci && npm run dev -- --port {{ port }})
+	cargo run -- {{ data_dir }}

templates/base.html
@@ -1,0 +1,36 @@
+<!DOCTYPE HTML>
+<html>
+  <head>
+    <style>
+      body {
+        width: 100%;
+        margin: 0;
+      }
+      nav {
+        width: 100%;
+        border-bottom: 0.25rem solid black;
+        border-color: black;
+        padding: 1rem 0;
+      }
+      nav a {
+        padding: 0 1rem;
+      }
+      main {
+        margin: auto;
+        max-width: 100rem;
+      }
+    </style>
+    {% block head %}
+    {% endblock head %}
+  </head>
+  <body>
+    <nav>
+      <!-- I can't be bothered to make a stylesheet -->
+      <a href="/"> Home </a>
+    </nav>
+    <main>
+      {% block content %} 
+      {% endblock content %}
+    </main>
+  </body>
+</html>

templates/entities.html
@@ -1,0 +1,9 @@
+{% extends "base.html" %}
+{% block content %}
+<h1> Entities: </h1>
+<ul>
+  {% for entity in entities %}
+    <li> <a href="/e/{{entity.name}}"> {{ entity.name }} </a> </li>
+  {% endfor %}
+</ul>
+{% endblock content %}

templates/index.html
@@ -1,0 +1,7 @@
+{% extends "base.html" %}
+{% block head %}
+{% endblock head %}
+
+{% block content %}
+<h1> Home </h1>
+{% endblock content %}

templates/repositories.html
@@ -1,0 +1,9 @@
+{% extends "base.html" %}
+{% block content %}
+<h1> Repositories: </h1>
+<ul>
+  {% for repo in repositories %}
+    <li> <a href="/r/{{entity_name}}/{{repo.name}}"> {{ repo.name }} </a> </li>
+  {% endfor %}
+</ul>
+{% endblock content %}

templates/repository.html
@@ -1,0 +1,51 @@
+{% extends "base.html" %}
+{% block head %}
+<style>
+  .commit {
+    margin: 1rem 0;
+    padding: 0;
+  }
+  .commit > div {
+    border: 0.25rem solid black;
+  }
+  .commit-header {
+    display: flex;
+    border-bottom: 0 !important;
+    flex-direction: row;
+  }
+  .commit-header > * {
+    margin: 0.2rem;
+  }
+  .commit-title {
+    width: 100%;
+  }
+  .commits {
+    list-style: none;
+    margin: 0;
+    padding: 0;
+  }
+  main > * {
+    width: 100%;
+  }
+</style>
+{% endblock head %}
+{% block content %}
+  <h1> <code>{{ entity_name }} / {{ repository_name }} </code>: </h1>
+  <ul class="commits">
+    {% for commit in commits %}
+      <li>
+        <div class="commit">
+          <div class="commit-header">
+            <code class = "commit-title" style="text-align: left;"> <strong> {{ commit.message_header }} </strong> </code>
+            <code style="text-align: right;"> {{ commit.commit_id | truncate(length=8, end="") }} </code>
+          </div>
+          <div>
+            <code style="white-space-collapse: preserve"> {{ commit.message_body | trim }} </code>
+          </div>
+        </div>
+      </li>
+    {% endfor %}
+  </ul>
+  <a href="/r/{{ entity_name }}/{{ repository_name }}?increment={% if increment - 10 > 0 %}{{ increment - 10 }}{% else %}0{% endif %}&rev={{ commit_id | default(value=commits.0.commit_id) }}"> Prev </a>
+  <a href="/r/{{ entity_name }}/{{ repository_name }}?increment={{ increment + 10}}&rev={{ commit_id | default(value=commits.0.commit_id) }}"> Next </a>
+{% endblock content %}

web-frontend/.gitignore
@@ -1,10 +1,0 @@
-.DS_Store
-node_modules
-/build
-/.svelte-kit
-/package
-.env
-.env.*
-!.env.example
-vite.config.js.timestamp-*
-vite.config.ts.timestamp-*

commit 372e5d2435862f2c03c5e4f384e2a2fb29b88fe5

fix(flake): remove nodejs unused packages
fixes: ea7819b

flake.nix
@@ -56,10 +56,6 @@
             pkgs.rust-analyzer
             pkgs.clippy
             pkgs.rustfmt
-            pkgs.nodejs
-            pkgs.nodePackages_latest.typescript-language-server
-            pkgs.nodePackages_latest.svelte-language-server
-            pkgs.nodePackages_latest.svelte-check
             pkgs.just
             pkgs.watchexec
           ]